1ce60dfb8e7adce99e3ff7e0ad435abc54237a8b
Orphée Lié <lieloumloum@gmail.com>
2024-08-16 23:36:39 +0200

    refator add and update attribue

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -14,6 +14,7 @@ use App\Models\Component;
 use Illuminate\Http\Response;
 use App\Models\GroupeAttribute;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AttributeController extends Controller
 {
@@ -106,9 +107,9 @@ class AttributeController extends Controller
                 $data_att['component_id'] = Component::where(['lib' => $request->json()->all()['component_id']])->first()->id;
             }
 
-            // Créer une nouvelle instance de l'attribut
-            $attribute = Attribute::where(['tech_name' => $request->json()->all()['tech_name']])->first();
+
                 # code...
+            $data_att['tech_name'] = Str::slug($request->json()->all()['tech_name'] , '-'). time();
             $attribute = Attribute::create($data_att);
             if (empty($request->json()->all()['is_lang']) or $request->json()->all()['is_lang'] == true) {
                 # code...
@@ -213,7 +214,7 @@ class AttributeController extends Controller
      */
     public function put(Request $request, string $id)
     {
-        try {
+        // try {
             // Valider les données JSON de la requête
             $validator = Validator::make($request->json()->all(), [
                 'tech_name' => 'required|string',
@@ -243,11 +244,11 @@ class AttributeController extends Controller
             }
 
             $data_att = $request->json()->all();
-            if (empty($request->json()->all()['classe_id'])) {
+            if (empty($request->json()->all()['class_id'])) {
                 # code...
                 $class = GroupeAttribute::findOrFail($request->json()->all()['groupe_attribute_id']);
                 // dd($class->classe_id);
-                $data_att['classe_id'] = $class->classe_id;
+                $data_att['class_id'] = $class->classe_id;
             }
 
             if (empty($request->json()->all()['component_id_multi'])) {
@@ -281,6 +282,7 @@ class AttributeController extends Controller
             $clnew = false;
             $class_sub = null;
             $gac = Component::where(['lib' => 'com.webtinix.infusio.GroupeAttributes'])->first();
+            $instance_delete = [];
                 //on verifie si la clé  instance_src existe dans le json de la requête
                 if (!empty($request->json()->all()['instance_src']) && $request->json()->all()['instance_src'] != "") {
                     $data = json_decode($request->json()->all()['instance_src'],true);
@@ -306,23 +308,31 @@ class AttributeController extends Controller
                         }
                     }
                     //create du groupe de l'attribut
-
+                    
                     foreach ($data['values'] as $key_ => $value) {
+                        // dd($value);
+                        $new_instance = true;
                         if (!empty($value['id'])) {
                             # code...
                             $instance = Instance::where(['id' => $value['id']])->first();
+                            $new_instance = false;
                         }else {
                             # code...
                             $instance = Instance::create([
                                 'classe_id' => $class_sub->id,
                             ]);
+
                         }
+                        
                         foreach ($value as $key => $val) {
+                            // dd($value);
                             if ($key == 'id') {
                                 continue;
                             }
-                        ///-----creation de l'instance-----
+                            // dd($val);:
+                            ///-----creation de l'instance-----
                             $attribute_sub = Attribute::where(['tech_name' => $key])->first();
+                            // dd($attribute_sub);
                             if (empty($attribute_sub)) {
                                 $attribute_sub = Attribute::create([
                                     'lib' => $val['lib'],
@@ -337,17 +347,18 @@ class AttributeController extends Controller
                             }
                             //-----creation du data----------
                             $dt = Data::where(['attribute_id' => $attribute_sub->id, 'class_id' => $class_sub->id, 'instance_id' => $instance->id])->first();
-
+                            // dd($dt);
                             // Si on nous demande de supprimer l'instance
                             if (isset($val['deleted']) && !empty($val['deleted']) && $val['deleted'] == 1) {
-                                if (!empty($dt)) {
-                                    # code...
-                                    $dt->delete();
-                                }
-                                $instance->delete();
-                                // # code...
+                                
+                                    if ($dt != null) {
+                                        # code...
+                                        $dt->delete();
+                                    }
+                                    $instance_delete [] = $instance;
+                                
                             }else{
-                                if (empty($dt)) {
+                                if ($dt == null) {
                                     $value_sub = Data::create([
                                         'attribute_id' => $attribute_sub->id,
                                         'class_id' => $class_sub->id,
@@ -359,16 +370,20 @@ class AttributeController extends Controller
                                     $dt->save();
                                 }
                             }
+                            
+                            
                         }
+                        // die();
                     }
+                    // dd( $instance_delete);
                     
                 }
             // Retourner une réponse JSON avec un message de succès
                 return response()->json(['code' => 200, 'message' => 'Attribut mis à jour avec succès.'], Response::HTTP_OK);
-        } catch (\Exception $e) {
-            // En cas d'erreur, retourner une réponse avec un message d'erreur
-            return response()->json(['code' => 404, 'message' => 'Une erreur est survenue lors de la mise à jour de l\'attribut.'], Response::HTTP_NOT_FOUND);
-        }
+        // } catch (\Exception $e) {
+        //     // En cas d'erreur, retourner une réponse avec un message d'erreur
+        //     return response()->json(['code' => 404, 'message' => 'Une erreur est survenue lors de la mise à jour de l\'attribut.'], Response::HTTP_NOT_FOUND);
+        // }
     }
 
     /**
